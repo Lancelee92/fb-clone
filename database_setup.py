decd7f19daee61530aab6be74ba57892a114e961
yclee <lancelee92@gmail.com>
2019-05-26 16:53:30 +0800

    feat: Set up postgreSQL database

---
+++ database_setup.py
@@ -0,0 +1,17 @@
+import os
+import sysconfig
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+engine = create_engine('postgresql://fb:password@localhost/facebook')
+Base.metadata.create_all(engine)
